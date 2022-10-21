f9961caeb1c846cb621c2169e22ae955d7df982e
jonas <jonasmpdc@gmail.com>
2022-10-20 23:47:54 -0300

    adding more files

--- trainingTypes.hs
+++ trainingTypes.hs
@@ -30,3 +30,42 @@ module TrainingTypes where
     functionB y = length y
 
     makingItWork = myFunc functionA functionB "memess" (True, "suck my tiny ")
+
+    i :: a -> a
+    i x = x
+
+    c :: a -> b -> a
+    c x y = x
+
+    c'' :: b -> a -> b
+    c'' x y = x
+
+    c' :: a -> b -> b
+    c' x y = y
+
+    r :: [a] -> [a]
+    r x = x
+
+    r' :: [a] -> [a]
+    r' x = x ++ x
+
+    ca :: b -> c
+    ca x = undefined
+
+    cb :: a -> b
+    cb x = undefined
+
+    co :: (b -> c) -> (a -> b) -> a -> c
+    co ca cb x = (ca (cb x))
+
+    aa :: a -> c
+    aa x = undefined
+
+    a :: (a -> c) -> a -> a
+    a _ x = x 
+
+    a'a :: a -> b
+    a'a x = undefined
+
+    a' :: (a -> b) -> a -> b
+    a' a'a x = (a'a x)
