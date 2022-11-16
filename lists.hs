b8be20701b87d24d23a3bf97185ecccc5d9bf32c
jonas <jonasmpdc@gmail.com>
2022-11-16 20:53:19 -0300

    minor changes

--- lists.hs
+++ lists.hs
@@ -68,60 +68,80 @@ eftIntBetter :: Int -> Int -> [Int]
 eftIntBetter x y = case compare x y of
   EQ -> [x]
   GT -> []
-  LT -> x : eftIntBetter (x + 1) y  
+  LT -> x : eftIntBetter (x + 1) y
 
 eftIntBetter2 :: Int -> Int -> [Int]
 eftIntBetter2 x y = case compare x y of
   EQ -> [x]
   GT -> []
-  LT -> x : eftIntBetter2 (succ x) y    
-
+  LT -> x : eftIntBetter2 (succ x) y
 
 eftChar :: Char -> Char -> [Char]
 eftChar x y = case compare x y of
   EQ -> [x]
   GT -> []
-  LT -> x : eftChar (succ x) y    
+  LT -> x : eftChar (succ x) y
 
 -- >>> eftIntBetter 1 10
 -- [1,2,3,4,5,6,7,8,9,10]
 
 enumFromToAnything :: (Ord a, Enum a) => a -> a -> [a]
-enumFromToAnything x y  = case compare x y of
+enumFromToAnything x y = case compare x y of
   EQ -> [x]
   GT -> []
-  LT -> x : enumFromToAnything (succ x) y    
+  LT -> x : enumFromToAnything (succ x) y
 
 myWords :: [Char] -> [[Char]]
 myWords x = go x []
   where
-    go x acc 
-        | (takeWhile (/= ' ') x == x) && x == "" = acc
-        | length acc == 0 = go (dropWhile (/= ' ') x) ("wallfish" : acc)
-        | takeWhile (/= ' ') x == "" = go (dropWhile (== ' ') x) acc
-        | takeWhile (/= ' ') x == x = go (dropWhile (/= ' ') x) (acc ++ [takeWhile (/= ' ') x])
-        | otherwise = go (dropWhile (/= ' ') x) (acc ++ [takeWhile (/= ' ') x])
+    go x acc
+      | (takeWhile (/= ' ') x == x) && x == "" = acc
+      | length acc == 0 = go (dropWhile (/= ' ') x) ("wallfish" : acc)
+      | takeWhile (/= ' ') x == "" = go (dropWhile (== ' ') x) acc
+      | takeWhile (/= ' ') x == x = go (dropWhile (/= ' ') x) (acc ++ [takeWhile (/= ' ') x])
+      | otherwise = go (dropWhile (/= ' ') x) (acc ++ [takeWhile (/= ' ') x])
 
 celsoSplit :: Char -> String -> [String]
 celsoSplit char text =
-    let
-        start = takeWhile (/= char) text
-        rest = drop 1 . dropWhile (/= char) $ text
-    in
-        if char `elem` rest then
-            start : celsoSplit char rest
-        else
-            [start, rest]
+  let start = takeWhile (/= char) text
+      rest = drop 1 . dropWhile (/= char) $ text
+   in if char `elem` rest
+        then start : celsoSplit char rest
+        else [start, rest]
 
-mySplit :: [Char] -> Char -> [[Char]]
-mySplit input separator = go input separator []
+mySplit :: Char -> [Char] -> [[Char]]
+mySplit separator input = go input separator []
   where
     go input separator acc
-        | (takeWhile (/= separator) input == input) && input == "" = acc
-        | takeWhile (/= separator) input == "" = go (dropWhile (== separator) input) separator acc
-        | takeWhile (/= separator) input == input = go (dropWhile (/= separator) input) separator (acc ++ [takeWhile (/= separator) input])
-        |otherwise = go (dropWhile (/= separator) input) separator (acc ++ [takeWhile (/= separator) input])
+      | (takeWhile (/= separator) input == input) && input == "" = acc
+      | takeWhile (/= separator) input == "" = go (dropWhile (== separator) input) separator acc
+      | takeWhile (/= separator) input == input = go (dropWhile (/= separator) input) separator (acc ++ [takeWhile (/= separator) input])
+      | otherwise = go (dropWhile (/= separator) input) separator (acc ++ [takeWhile (/= separator) input])
 
+myWords2 :: String -> [String]
+myWords2 =
+  mySplit ' '
+
+myLines :: String -> [String]
+myLines =
+  mySplit '\n'
+
+firstSen = "Tyger Tyger, burning bright\n"
+
+secondSen = "In the forests of the night\n"
+
+thirdSen = "What immortal hand or eye\n"
+
+fourthSen =
+  "Could frame thy fearful\
+  \ symmetry?"
+
+sentences = firstSen ++ secondSen ++ thirdSen ++ fourthSen
+
+shouldEqual = ["Tyger Tyger, burning bright", "In the forests of the night", "What immortal hand or eye", "Could frame thy fearful symmetry?"]
+
+-- >>> myLines sentences == shouldEqual
+-- True
 
 -- >>>"wallfish" ++ dropWhile (/= ' ') "sheryl wants fun"
 -- "wallfish wants fun"
