197d296980acde15c06fd2ee71e64760766351e6
jonas <jonasmpdc@gmail.com>
2023-01-16 22:27:14 -0300

    finished some exercises

--- chapter10exercises.hs
+++ chapter10exercises.hs
@@ -3,13 +3,17 @@ module Chapter10Exercises where
     stops = "pbtdkg"
     vowels = "aeiou"
 
+    nouns = ["Jonas", "cars", "carrots"]
+
+    verbs = ["eats", "lives", "likes"]
+
     permutateOverVowels firstLetter lastLetter = 
-        map (\x -> (firstLetter, x, lastLetter)) vowels
+        map (\x -> (firstLetter, x, lastLetter)) verbs
 
     permutateStops (x:xs) = 
         case xs of
             [] -> []
-            _ -> map (\y -> (x, y, head xs)) vowels
+            _ -> map (\y -> (x, y, head xs)) verbs
 
     fst' (a,b,c) = a
     
@@ -21,4 +25,11 @@ module Chapter10Exercises where
     permutateStopsOverVowelsOnlyP list@(x:xs) acc = 
         case xs of
             [] -> foldr (\current accumulator -> if fst' (head current) == 'p' then current : accumulator else accumulator) [] acc
-            _ -> permutateStopsOverVowels xs (permutateStops list : acc)
+            _ -> permutateStopsOverVowelsOnlyP xs (permutateStops list : acc)
+
+    seekritFunc x =
+        div (sum (map length (words x)))
+        (length (words x))
+
+    seekritFuncFractional x =
+        (fromIntegral $ (sum (map length (words x)))) / (fromIntegral $ (length (words x)))
