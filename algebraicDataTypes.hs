adbbbc4863c390d292f0a931cc9f8c87709b207c
jonas <jonasmpdc@gmail.com>
2023-01-22 22:45:14 -0300

    finished what's a type and what's data

--- algebraicDataTypes.hs
+++ algebraicDataTypes.hs
@@ -10,26 +10,33 @@ module AlgebraicDataTypes where
 
     data Price = Price Integer deriving (Eq, Show)
 
+    data Size = Size Integer deriving (Eq, Show)
+
     data Manufacturer = Mini | Mazda | Tata deriving (Eq, Show)
 
     data Airline = PapuAir | CatapultsR'Us | TakeYourChancesUnited deriving (Eq,Show)
 
-    data Vehicle = Car Manufacturer Price | Plane Airline deriving (Eq, Show)
+    data Vehicle = Car Manufacturer Price | Plane Airline Size deriving (Eq, Show)
 
     myCar = Car Mini (Price 14000)
     urCar = Car Mazda (Price 20000)
     clownCar = Car Tata (Price 7000)
-    doge = Plane PapuAir
+    doge = Plane PapuAir (Size 1)
 
-    cars = [myCar,urCar,doge]
+    cars = [myCar, urCar, doge]
 
     isCar :: Vehicle -> Bool
     isCar (Car _ _)= True
     isCar _ = False
 
     isPlane :: Vehicle -> Bool
-    isPlane (Plane _)= True
+    isPlane (Plane _ (Size _))= True
     isPlane _ = False
 
     areCars :: [Vehicle] -> Bool
-    areCars = foldr (\x acc -> if isCar x then True else acc) False
+    areCars [] = True
+    areCars xs = foldr ((&&) . isCar) True xs
+
+    getManufacturer :: Vehicle -> Maybe Manufacturer
+    getManufacturer (Car x b) = Just x
+    getManufacturer _ = Nothing
