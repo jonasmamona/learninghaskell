677bd1e413ee4c9336b71c6b8c9c6079adb6b3d0
jonas <jonasmpdc@gmail.com>
2024-01-20 14:13:18 -0300

    back at it, doing reader again

--- chapter22.hs
+++ chapter22.hs
@@ -1,15 +1,20 @@
 module Chapter22 where
 
 import Control.Applicative
+import Data.Char
+import Control.Monad.Trans.Reader
+import Control.Monad
 
+boop :: Num a => a -> a
 boop = (*2)
 
+doop :: Num a => a -> a
 doop = (+10)
 
-bip :: Integer -> Integer
+bip :: Num a => a -> a
 bip = boop . doop
 
-bloop :: Integer -> Integer
+bloop :: Num a => a -> a
 bloop = fmap boop doop
 
 bbop :: Integer -> Integer
@@ -18,3 +23,35 @@ bbop = (+) <$> boop <*> doop
 duwop :: Integer -> Integer
 duwop = liftA2 (+) boop doop
 
+boopDoop :: Integer -> Integer
+boopDoop = do
+  a <- boop
+  b <- doop
+  return (a + b)
+
+cap :: [Char] -> [Char]
+cap = map toUpper
+
+rev :: [Char] -> [Char]
+rev = reverse
+
+composed :: [Char] -> [Char]
+composed = cap . rev
+
+fmapped :: [Char] -> [Char]
+fmapped = fmap cap rev
+
+myWay :: [Char] -> ([Char], [Char])
+myWay x = (cap x, rev x)
+
+tupled :: [Char] -> ([Char], [Char])
+tupled = (,) <$> cap <*> rev
+
+tupledMonad :: [Char] -> ([Char], [Char])
+tupledMonad = do
+  a <- cap
+  b <- rev
+  return (a, b)
+
+tupledMonadWithBind:: [Char] -> ([Char], [Char])
+tupledMonadWithBind = cap >>= (\x -> rev >>= (\y -> return (x, y)))
